fb6f5da7735ce5dafa7451e62aa6d3c960751cb9
ERROR4-04 <75242887+ERROR4-04@users.noreply.github.com>
2021-02-03 20:16:46 +0300

    Create operations.h

---
+++ operations.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef OPERATORS_H	
+#define OPERATORS_H
+int add(int x, int y) {
+	return x + y;
+}
+int sub(int x, int y) {
+	return x - y;
+}
+
+int mult(int x, int y) {
+	return x * y;
+}
+int divi(int x, int y) {
+	return x / y;
+}
+int mod(int x, int y) {
+	return x % y;
+}
+#endif
